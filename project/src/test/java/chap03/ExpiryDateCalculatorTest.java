37101788b7c8dffde50ac37ec019b6824c666574
sanhee <68936@naver.com>
2021-11-27 00:21:08 +0900

    refactor: 만료일 계산 로직 메서드화

--- project/src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ project/src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -7,19 +7,21 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExpiryDateCalculatorTest {
+
     @Test
     void 만원_납부하면_한달_뒤가_만료일이_됨() {
-        LocalDate billingDate = LocalDate.of(2021, 11, 1);
-        int payAmount = 10_000;
+        assertExpiryDate(
+                LocalDate.of(2021, 11, 1), 1000,
+                LocalDate.of(2021, 12, 1));
+        assertExpiryDate(
+                LocalDate.of(2021, 11, 5), 1000,
+                LocalDate.of(2021, 12, 5));
+    }
 
+    private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expiryDate) {
         ExpiryDateCalculator cal = new ExpiryDateCalculator();
-        LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+        LocalDate actualExpiryDate = cal.calculateExpiryDate(billingDate, payAmount);
 
-        assertEquals(LocalDate.of(2021, 12, 1), expiryDate);
-
-        LocalDate billingDate2 = LocalDate.of(2021, 11, 5);
-        LocalDate expiryDate2 = cal.calculateExpiryDate(billingDate2, payAmount);
-
-        assertEquals(LocalDate.of(2021, 12, 5), expiryDate2);
+        assertEquals(actualExpiryDate, expiryDate);
     }
 }

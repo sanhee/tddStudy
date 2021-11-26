e454cc6f588ae9b33c6d7abaada3f087409ddf34
sanhee <68936@naver.com>
2021-11-27 00:04:47 +0900

    feat: 1만원을 납부하면 한 달뒤가 만료일

---
+++ project/src/main/java/chap03/ExpiryDateCalculator.java
@@ -0,0 +1,10 @@
+package chap03;
+
+import java.time.LocalDate;
+
+public class ExpiryDateCalculator {
+
+    public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
+        return LocalDate.of(2021, 12, 1);
+    }
+}
---
+++ project/src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -0,0 +1,20 @@
+package chap03;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExpiryDateCalculatorTest {
+    @Test
+    void 만원_납부하면_한달_뒤가_만료일이_됨() {
+        LocalDate billingDate = LocalDate.of(2021, 11, 1);
+        int payAmount = 10_000;
+
+        ExpiryDateCalculator cal = new ExpiryDateCalculator();
+        LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+
+        assertEquals(LocalDate.of(2021, 12, 1), expiryDate);
+    }
+}

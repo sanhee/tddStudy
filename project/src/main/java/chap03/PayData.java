31e6557fe1551573fed1e59074671f8e9b7a2d83
sanhee <68936@naver.com>
2021-11-27 19:50:56 +0900

    feat: 첫 납부일 예외상황으로 인해 PayData 클래스 정의
    호출 파라미터 갯수가 3개가 되므로, 객체로 뺌

---
+++ project/src/main/java/chap03/PayData.java
@@ -0,0 +1,42 @@
+package chap03;
+
+import java.time.LocalDate;
+
+public class PayData {
+    private LocalDate billingDate;
+    private int payAmount;
+
+    // 기본 생성자, 인스턴스화 막기
+    private PayData() {
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public LocalDate getBillingDate() {
+        return billingDate;
+    }
+
+    public int getPayAmount() {
+        return payAmount;
+    }
+
+    public static class Builder {
+        private final PayData data = new PayData();
+
+        public Builder billingDate(LocalDate billingDate) {
+            data.billingDate = billingDate;
+            return this;
+        }
+
+        public Builder payAmount(int payAmount) {
+            data.payAmount = payAmount;
+            return this;
+        }
+
+        public PayData build() {
+            return data;
+        }
+    }
+}

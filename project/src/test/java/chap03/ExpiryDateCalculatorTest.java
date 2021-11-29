86da9a5a309e5a92a55090378a29f20fdfdebd5d
sanhee <68936@naver.com>
2021-11-29 19:57:02 +0900

    feat: 10만원 지불할 경우 12개월 연장

--- project/src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ project/src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -102,4 +102,15 @@ public class ExpiryDateCalculatorTest {
                  LocalDate.of(2019,4,30)
          );
     }
+
+    @Test
+    void 십만원을_납부하면_1년_제공(){
+        assertExpiryDate(
+                PayData.builder()
+                       .billingDate(LocalDate.of(2019,1,28))
+                       .payAmount(100_000)
+                       .build(),
+                LocalDate.of(2020,1,28)
+        );
+    }
 }
